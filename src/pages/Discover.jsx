4afbaed5c16b6d3593eebb4be3ef89b0bc0f3ca1
Dullah Mtavya <mtavya420@gmail.com>
2023-02-22 13:57:40 +0900

    commit some changes

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,32 +1,65 @@
-import {Error,Loader,  SongCard} from '../components' ;
-import {genres} from '../assets/constants';
+import React from "react";
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
+import PlayPause from "../components/PlayPause";
+import { playPause, setActiveSong } from "../redux/features/playerSlice";
 
-const Discover = () => {
-const genreTitle = 'Pop';
+const SongCard = ({ song, isPlaying, activeSong, data, i }) => {
+  const dispatch = useDispatch();
 
-return (
-  <div className="flex flex-col">
-    <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
-      <h2 className="font-bold text-3xl text-white text-left">Discover {genreTitle}</h2>
-      <select
-        onChange={()=> {}}
-        value=""
-        className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5"
-      >
-        {genres.map((genre)=><option key={genre.value} value={genre.value}>{genre.title}</option>)}
-      </select>
-    </div>
-    <div className='flex flex-wrap sm:justify-start justify-center gap-8'>
-      {[1,2,3,4,5,6,7,8,9,10].map((song,i) =>(
-        <SongCard
-          key={song.key}
-          song={song}
-          i={i}
+  const handlePauseClick = () => {
+    dispatch(playPause(false));
+  };
+
+  const handlePlayClick = () => {
+    dispatch(setActiveSong({ song, data, i }));
+    dispatch(playPause(true));
+  };
+
+  return (
+    <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
+      <div className="relative w-full h-56 group">
+        <div
+          className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex ${
+            activeSong?.title === song.title
+              ? "flex bg-black bg-opacity-70"
+              : "hidden"
+          }`}
+        >
+          <PlayPause
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            song={song}
+            handlePause={handlePauseClick}
+            handlePlay={handlePlayClick}
           />
-      ))}
+        </div>
+        <img
+          alt="song_img"
+          src={song.images?.coverart}
+          className="w-full h-full rounded-lg"
+        />
       </div>
-  </div>
-);
+
+      <div className="mt-4 flex flex-col">
+        <p className="font-semibold text-lg text-white truncate">
+          <Link to={`/songs/${song?.key}`}>{song.title}</Link>
+        </p>
+        <p className="text-sm truncate text-gray-300 mt-1">
+          <Link
+            to={
+              song.artists
+                ? `/artists/${song?.artists[0]?.adamid}`
+                : "/top-artists"
+            }
+          >
+            {song.subtitle}
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
 };
-export default Discover;
+
+export default SongCard;
